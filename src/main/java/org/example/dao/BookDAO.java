e3b05cd0e8550f4a790fa53720722177c4787f8c
AntonP17 <pokrovskii.anton@mail.ru>
2025-01-27 21:44:39 +0300

    Реализация CRUD
    реализовал круд методы, конроллеры, DAO , подключил к бд новой

--- src/main/java/org/example/dao/BookDAO.java
+++ src/main/java/org/example/dao/BookDAO.java
@@ -1,4 +1,49 @@
 package org.example.dao;
 
+import org.example.model.Book;
+import org.example.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
 public class BookDAO {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public BookDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    // получение всех книг
+    public List<Book> index() {
+        return jdbcTemplate.query("SELECT * FROM Book", new BeanPropertyRowMapper<>(Book.class));
+    }
+
+    // получение конкретной книги
+    public Book show(int id) {
+        return jdbcTemplate.query("SELECT * FROM Book WHERE id=?", new Object[]{id}, new BeanPropertyRowMapper<>(Book.class))
+                .stream().findAny().orElse(null);
+    }
+
+    // сохранение
+    public void save(Book book) {
+        jdbcTemplate.update("INSERT INTO Book (title, author, year) VALUES(?, ?, ?)", book.getTitle(), book.getAuthor(), book.getYear());
+    }
+
+    // обновление
+    public void update(int id, Book updatedBook) {
+        jdbcTemplate.update("UPDATE Book SET tittle=?, author=?, year=? WHERE id=?",
+                updatedBook.getTitle(), updatedBook.getAuthor(), updatedBook.getYear(), id);
+    }
+
+    // удаление
+    public void delete(int id) {
+        jdbcTemplate.update("DELETE FROM Book WHERE id=?", id);
+    }
+
 }
--- src/main/java/org/example/dao/PersonDAO.java
+++ src/main/java/org/example/dao/PersonDAO.java
@@ -1,4 +1,55 @@
 package org.example.dao;
 
+import org.example.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Component;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
 public class PersonDAO {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public PersonDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    // получаем всех
+    public List<Person> index() {
+        return jdbcTemplate.query("SELECT * FROM Person", new BeanPropertyRowMapper<>(Person.class));
+    }
+
+    // получаем одного
+    public Person show(int id) {
+        return jdbcTemplate.query("SELECT * FROM Person WHERE id=?", new Object[]{id}, new BeanPropertyRowMapper<>(Person.class))
+                .stream().findAny().orElse(null);
+    }
+
+    // сохраняем
+    public void save(Person person) {
+        jdbcTemplate.update("INSERT INTO Person (firstname, lastname, patronymic, dayOfBirth) VALUES (?, ?, ?, ?)",
+                person.getFirstName(), person.getLastName(), person.getPatronymic(), person.getDayOfBirth());
+    }
+
+    // обновляем
+    public void update(int id, Person updatedPerson) {
+        jdbcTemplate.update("UPDATE Person SET firstname=?, lastname=?, patronymic=?, dayOfBirth=? WHERE id=?",
+                updatedPerson.getFirstName(), updatedPerson.getLastName(),
+                updatedPerson.getPatronymic(), updatedPerson.getDayOfBirth(), id);
+    }
+
+    // удаляем
+    public void delete(int id) {
+        jdbcTemplate.update("DELETE FROM Person WHERE id=?", id);
+    }
+
 }
